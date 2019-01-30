62b0f20400e45bf4e2c8cad48fc20994ca494854
Ransford Walker <ransfordw@gmail.com>
2019-01-30 14:32:36 -0500

    added some notes

--- Properties/Person.cs
+++ Properties/Person.cs
@@ -5,8 +5,19 @@ namespace Properties
     public class Person
     {
         //This private acessor makes it so that the Birthdate cannot be set outside of this class. So we need to set it in the constructor here.
+
+        //Store auto generated properties on the top
+        public string Name { get; set; }
+        public string UserName { get; set; }
         public DateTime Birthdate { get; private set; }
 
+        //Have constructors in the middle as a vertical separator.
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+        //Store calculated properties on the bottom
         public int Age
         {
             get
@@ -15,11 +26,6 @@ namespace Properties
                 var _age = timeSpan.Days / 365;
                 return _age;
             }
-        }
-
-        public Person(DateTime birthdate)
-        {
-            Birthdate = birthdate;
         }
     }
 }
