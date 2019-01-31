9729ad224607962f8148cc6618a1cfcf98b10219
Ransford Walker <ransfordw@gmail.com>
2019-01-31 16:09:17 -0500

    addded some composition notes

--- Composition/Logger.cs
+++ Composition/Logger.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 
 namespace Composition
-{
+{ 
+    // Inheritance is more of a tightly coupled relationship.
+    // Composition is loose coupling with more flexibility.
+
+
+    // This Logger class is functioning simmilarly to a repository, except that is is a required parameter of the 'derived' classes constructors
     public class Logger
     {
         public void Log(string message)
