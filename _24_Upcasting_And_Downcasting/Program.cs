9d15be654c785096678030c06404b563c95607c4
Ransford Walker <ransfordw@gmail.com>
2019-07-11 09:18:37 -0400

    commit before adding wpf

--- _24_Upcasting_And_Downcasting/Program.cs
+++ _24_Upcasting_And_Downcasting/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 namespace _24_Upcasting_And_Downcasting
 {
@@ -10,14 +13,28 @@ namespace _24_Upcasting_And_Downcasting
             // Conversion from a base class to a derived class (downcasting)
             // as and is keywords
 
-            Text text = new Text();
-            Shape shape = text; // both are references to the same object in memory, but with different views
-            text.Width = 200;
-            shape.Width = 100;
-            Console.WriteLine(text.Width);
-            Console.ReadKey();
-             
+            // Text text = new Text();
+            // Shape shape = text; // both are references to the same object in memory, but with different views
+            // text.Width = 200;
+            // shape.Width = 100;
+            // Console.WriteLine(text.Width);
 
+            // StreamReader reader = new StreamReader(new MemoryStream());
+            var list = new ArrayList
+            {
+                // Not Type safe
+                1,
+                "Mosh",
+                new Text()
+            };
+
+            var anotherList = new List<int>(); // A typed generic list
+
+            Shape shape = new Text();
+            Text text = (Text)shape;
+
+
+            Console.ReadKey();
         }
     }
 }
