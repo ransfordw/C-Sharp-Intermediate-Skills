0c13d23e8cd1b027c076a015ed353b2ec32f0e4f
Ransford Walker <ransfordw@gmail.com>
2019-07-11 16:29:44 -0400

    Finished method overriding lesson

---
+++ _28_Method_Overriding/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _28_Method_Overriding
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var shapes = new List<Shape>
+            {
+                new Circle(),
+                new Rectangle(),
+                new Triangle()
+            };
+
+            var canvas = new Canvas();
+            canvas.DrawShapes(shapes);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ _28_Method_Overriding/Shape.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _28_Method_Overriding
+{
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        //public Position Position { get; set; }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine("Draw a Shape.");
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            base.Draw();
+            Console.WriteLine("Draw a Circle");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a Rectangle.");
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a Triangle.");
+        }
+    }
+
+}
